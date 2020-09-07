0233dfea240c7ec0e59729098553c5d5ab74e72f
Yohanes Antonius <49809783+YohanesAntonius@users.noreply.github.com>
2020-09-08 00:31:10 +0700

    Create aksi.php

---
+++ aksi.php
@@ -0,0 +1,34 @@
+<!DOCTYPE>
+<html>
+<head>
+<style>
+	#myHeader {
+		background-color: lightblue;
+		color: black;
+		padding: 40px;
+		text-align: center;
+	}
+</style>	
+</head>
+<body>
+	<h1 id="myHeader">SOAL MATEMATIKA INFORMATIKA IV</h1>
+	<h1><center>
+	<form action="proses.php" method="POST">
+	<pre>
+	 Sp	 :<input type="text" name="sp"> 
+	Sq 	:<input type="text" name="sq">
+	p	:<input type="text" name="p">
+	q 	:<input type="text" name="q">
+
+	Ditanya:
+<input type="submit" name="silodis" value="Silogisme Disjungtif" />
+<input type="submit" name="dilema" value="Dilema" />
+<input type="submit" name="silokat" value="Silogisme Kategorik" />
+	   	 
+		 </pre>
+	</form>					
+	</center></h1>
+</style>
+
+</body>
+</html>s
